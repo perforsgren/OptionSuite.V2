fb48e56ef823025703c7a31ed299e7daaf48e112
perforsgren <perforsgren@icloud.com>
2025-12-19 14:49:47 +0100

    New

--- OptionSuite.Blotter.Wpf/Views/BlotterRootView.xaml.cs
+++ OptionSuite.Blotter.Wpf/Views/BlotterRootView.xaml.cs
@@ -11,38 +11,5 @@ namespace OptionSuite.Blotter.Wpf.Views
             InitializeComponent();
         }
 
-        private void OptionsGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (DataContext is not BlotterRootViewModel vm) return;
-
-            // När en option trade selectas, cleara om det var en linear trade
-            if (e.AddedItems.Count > 0 && e.AddedItems[0] is TradeRowViewModel selectedTrade)
-            {
-                // Sätt den nya selectionen direkt
-                vm.SelectedTrade = selectedTrade;
-            }
-            else if (e.RemovedItems.Count > 0 && e.AddedItems.Count == 0)
-            {
-                // Användaren deselekterade manuellt
-                vm.SelectedTrade = null;
-            }
-        }
-
-        private void LinearGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (DataContext is not BlotterRootViewModel vm) return;
-
-            // När en linear trade selectas, cleara om det var en option trade
-            if (e.AddedItems.Count > 0 && e.AddedItems[0] is TradeRowViewModel selectedTrade)
-            {
-                // Sätt den nya selectionen direkt
-                vm.SelectedTrade = selectedTrade;
-            }
-            else if (e.RemovedItems.Count > 0 && e.AddedItems.Count == 0)
-            {
-                // Användaren deselekterade manuellt
-                vm.SelectedTrade = null;
-            }
-        }
     }
 }
