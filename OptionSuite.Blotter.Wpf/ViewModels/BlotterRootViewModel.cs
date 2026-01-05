87332ec1bc2659dc3e9ccfdb2ccb4d2629e71308
perforsgren <perforsgren@icloud.com>
2026-01-05 15:18:49 +0100

    Latest 7

--- OptionSuite.Blotter.Wpf/ViewModels/BlotterRootViewModel.cs
+++ OptionSuite.Blotter.Wpf/ViewModels/BlotterRootViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Windows.Input;
 using System.Windows.Threading;
