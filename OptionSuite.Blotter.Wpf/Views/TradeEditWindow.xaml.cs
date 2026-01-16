f7b16ed236328791736d3a117bde3c7377056adb
perforsgren <perforsgren@icloud.com>
2026-01-16 10:52:27 +0100

    20260116:3

--- OptionSuite.Blotter.Wpf/Views/TradeEditWindow.xaml.cs
+++ OptionSuite.Blotter.Wpf/Views/TradeEditWindow.xaml.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Windows;
+using System.Windows.Input;
 using OptionSuite.Blotter.Wpf.ViewModels;
 
 namespace OptionSuite.Blotter.Wpf.Views
@@ -8,6 +10,7 @@ namespace OptionSuite.Blotter.Wpf.Views
         public TradeEditWindow()
         {
             InitializeComponent();
+            StateChanged += OnStateChanged;
         }
 
         /// <summary>
@@ -17,6 +20,41 @@ namespace OptionSuite.Blotter.Wpf.Views
         {
             DialogResult = false;
             Close();
+        }
+
+        /// <summary>
+        /// Minimize button click handler.
+        /// </summary>
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Maximize/Restore button click handler.
+        /// </summary>
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized 
+                ? WindowState.Normal 
+                : WindowState.Maximized;
+        }
+
+        /// <summary>
+        /// Close button click handler.
+        /// </summary>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        /// <summary>
+        /// Update maximize icon when window state changes.
+        /// </summary>
+        private void OnStateChanged(object sender, EventArgs e)
+        {
+            // Could update the maximize icon here if needed
         }
 
         /// <summary>
